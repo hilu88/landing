fce81c6550b26208dcb5900b48f5b2cc7098e2da
Ostap <1idividedbyzero@gmail.com>
2018-05-03 00:55:33 +0300

    first commit

---
+++ js/animation.js
@@ -0,0 +1,80 @@
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin:'bottom'
+});
+sr.reveal('.header-info', {
+    duration: 2000,
+    origin:'right',
+    distance:'500px'
+});
+sr.reveal('.header-button', {
+    duration: 2000,
+    delay: 1000,
+    origin:'bottom'
+});
+sr.reveal('.about-us', {
+    duration: 2000,
+    origin:'bottom'
+});
+sr.reveal('.about-us', {
+    duration: 2000,
+    origin:'bottom'
+});
+sr.reveal('.skills-info', {
+    duration: 2000,
+    origin:'left',
+    distance:'300px',
+    viewFactor: 0.3
+});
+sr.reveal('.skills-block', {
+    duration: 2000,
+    origin:'right',
+    distance:'300px',
+    viewFactor: 0.3
+});
+sr.reveal('.skills-progress', {
+    duration: 3000,
+    delay: 500,
+    origin:'left',
+    distance:'300px',
+    viewFactor: 0.3
+});
+sr.reveal('#slider', {
+    duration: 2000,
+    origin:'top'
+});
+sr.reveal('.project-h2', {
+    duration: 2000,
+    origin:'top'
+});
+sr.reveal('.project-p', {
+    duration: 2000,
+    delay: 500,
+    origin:'top'
+});
+sr.reveal('.project-form-left', {
+    duration: 2000,
+    origin:'left',
+    distance:'500px'
+});
+sr.reveal('.project-form-right', {
+    duration: 2000,
+    origin:'right',
+    distance:'500px'
+});
+sr.reveal('.project-form-bottom1', {
+    duration: 2000,
+    origin:'bottom',
+    distance:'200px'
+});
+sr.reveal('.project-form-bottom2', {
+    duration: 2200,
+    origin:'bottom',
+    distance:'200px'
+});
+sr.reveal('.project-button', {
+    duration: 2000,
+    delay: 1000,
+    origin:'bottom'
+});
